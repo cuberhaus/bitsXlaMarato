539dabd3f9390827f3b199dbeffc94f781b2a6bb
cuberhaus <polcg10@gmail.com>
2022-12-17 18:45:31 +0100

    This is a placeholder

--- src/ImageViewer.py
+++ src/ImageViewer.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import filedialog, ttk
 import cv2
 from PIL import Image, ImageTk
-
+import MASKRCNN.inference as inf
 
 class VideoViewer:
     def __init__(self, root):
