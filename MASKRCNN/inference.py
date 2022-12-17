eb997abe4992a87ca4a725cfbe3e1a0cf6a9a1df
Pablesky <pablesky@live.com>
2022-12-17 18:12:57 +0100

    semiDone

--- MASKRCNN/inference.py
+++ MASKRCNN/inference.py
@@ -1,3 +1,4 @@
+import numpy
 from PIL import Image
 import matplotlib.pyplot as plt
 import torch
@@ -12,8 +13,7 @@ import warnings
 warnings.filterwarnings('ignore')
 
 #FOTO = 'Dataset/PNGImages/img3.png'
-FOTO = '../DATASETS/Dataset/test/img2res.png'
-
+FOTO = './MARATO/PNGImages/601_S2_40.jpg'
 #https://haochen23.github.io/2020/06/fine-tune-mask-rcnn-pytorch.html#.YsxbR-xBxH5
 
 def get_coloured_mask(mask):
@@ -29,9 +29,9 @@ def get_coloured_mask(mask):
     r = np.zeros_like(mask).astype(np.uint8)
     g = np.zeros_like(mask).astype(np.uint8)
     b = np.zeros_like(mask).astype(np.uint8)
-    r[mask == 1], g[mask == 1], b[mask == 1] = colours[random.randrange(0, 10)]
+    r[mask == 1], g[mask == 1], b[mask == 1] = colours[1]
     coloured_mask = np.stack([r, g, b], axis=2)
-    return coloured_mask
+    return np.stack([r, g, b], axis=2)
 
 
 def get_prediction(img_path, confidence):
@@ -55,7 +55,10 @@ def get_prediction(img_path, confidence):
     img = img.to(device)
     pred = model([img])
     pred_score = list(pred[0]['scores'].detach().cpu().numpy())
-    pred_t = [pred_score.index(x) for x in pred_score if x > confidence][-1]
+    try:
+        pred_t = [pred_score.index(x) for x in pred_score if x > confidence][-1]
+    except:
+        return numpy.array([]), numpy.array([]), numpy.array([])
     masks = (pred[0]['masks'] > 0.5).squeeze().detach().cpu().numpy()
     # print(pred[0]['labels'].numpy().max())
     pred_class = [CLASS_NAMES[i] for i in list(pred[0]['labels'].cpu().numpy())]
@@ -89,22 +92,116 @@ def segment_instance(img_path, confidence=0.5, rect_th=2, text_size=2, text_th=2
         img = cv2.addWeighted(img, 1, rgb_mask, 0.5, 0)
         #cv2.rectangle(img, boxes[i][0], boxes[i][1], color=(0, 255, 0), thickness=rect_th)
         #cv2.putText(img, pred_cls[i], boxes[i][0], cv2.FONT_HERSHEY_SIMPLEX, text_size, (0, 255, 0), thickness=text_th)
-    plt.figure(figsize=(20, 30))
-    plt.imshow(img)
-    plt.xticks([])
-    plt.yticks([])
-    plt.show()
+    #plt.figure(figsize=(20, 30))
+    #plt.imshow(img)
+    #plt.xticks([])
+    #plt.yticks([])
+    #plt.show()
+    return img
 
 
+# Program To Read video
+# and Extract Frames
 
-# set to evaluation mode
-#model = torch.load('pedestrians.pt')
-model = torch.load('ganchitos.pt')
-model.eval()
-#CLASS_NAMES = ['__background__', 'Ganchito']
-CLASS_NAMES = ['__background__', '']
-device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-model.to(device)
+import cv2
+import matplotlib.pyplot as plt
+import os
+import shutil
 
-segment_instance(FOTO, confidence=0.95)
+
+# Function to extract frames
+def Vid2Frame(path, filename):
+    vid_name = filename.split('.')
+    vid_name = vid_name[0]
+
+    # Creacion carpeta frames
+    full_path = (path + 'frames' + '_' + vid_name + '/')
+    if (os.path.exists(full_path)):
+        shutil.rmtree(full_path)
+    os.mkdir(full_path)
+
+    # Path to video file
+    vidObj = cv2.VideoCapture(path + filename)
+
+    n_frames = str(vidObj.get(cv2.CAP_PROP_FRAME_COUNT))
+    n_zeros = len(n_frames) - 2
+
+    # Used as counter variable
+    count = 0
+
+    # checks whether frames were extracted
+    success, image = vidObj.read()
+
+    while success:
+        # vidObj object calls read
+        # function extract frames
+
+        crop_img = image[84:380, 54:526]
+        final_image = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
+
+        ini_zeros = int(n_zeros - len(str(count))) * '0'
+        num = ini_zeros + str(count)
+
+        # Saves the frames with frame-count
+        # print(full_path + vid_name + "_" + num +".jpg", final_image)
+        cv2.imwrite(full_path + vid_name + "_" + num + ".jpg", final_image)
+
+        count += 1
+
+        success, image = vidObj.read()
+
+# Program To Read video
+# and Extract Frames
+
+import cv2
+import matplotlib.pyplot as plt
+import os
+import shutil
+
+# Function to extract frames
+def Frame2Vid(path):
+
+    frames = sorted(os.listdir(path))
+    img_array = []
+
+    # Leer imagenes
+    for x in range(0, len(frames)):
+        nomArchivo = frames[x]
+        dirArchivo = path + str(nomArchivo)
+        img = cv2.imread(dirArchivo)
+        img_array.append(img)
+
+    # Caracteríasticas video
+    name_vid = ('_').join(frames[0].split('_')[0:2])
+
+    height, width, layers = img_array[0].shape
+    size = (width, height)
+
+    out = cv2.VideoWriter(path + name_vid + '.avi', cv2.VideoWriter_fourcc(*'DIVX'), 15, size)
+
+    for i in range(len(img_array)):
+        out.write(img_array[i])
+    out.release()
+
+from glob import glob
+if __name__ == '__main__':
+    Vid2Frame('C:/Users/pable/Documents/GitHub/bitsXlaMarato/videos/', 'S2-603.avi')
+    fotos = glob('C:/Users/pable/Documents/GitHub/bitsXlaMarato/videos/frames_S2-603/*.jpg')
+    print(fotos)
+
+    for i, foto in enumerate(fotos):
+
+        # set to evaluation mode
+        #model = torch.load('pedestrians.pt')
+        model = torch.load('marato.pt')
+        model.eval()
+        #CLASS_NAMES = ['__background__', 'Ganchito']
+        CLASS_NAMES = ['__background__', '']
+        device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+        model.to(device)
+
+        foto = segment_instance(foto, confidence=0.90)
+        cv2.imwrite('C:/Users/pable/Documents/GitHub/bitsXlaMarato/videos/frames_588_short2/' + str(i) +'.jpg', foto)
+
+    Frame2Vid('C:/Users/pable/Documents/GitHub/bitsXlaMarato/videos/frames_588_short2/')
 
--- MASKRCNN/pederastrians.py
+++ MASKRCNN/pederastrians.py
@@ -3,12 +3,13 @@ import numpy as np
 import torch
 import torch.utils.data
 from PIL import Image
+import matplotlib.pyplot as plt
 
 modelo = 3
 
 if modelo == 0:
     DATASET = '../DATASETS/PennFudanPed'
-    SIZE  = -50
+    SIZE = -50
     MODELNAME = 'pedestrians.pt'
 elif modelo == 1:
     DATASET = '../DATASETS/Dataset'
@@ -19,7 +20,8 @@ else:
     SIZE = -40
     MODELNAME = 'marato.pt'
 
-#https://haochen23.github.io/2020/06/fine-tune-mask-rcnn-pytorch.html#.YsxbR-xBxH5
+
+# https://haochen23.github.io/2020/06/fine-tune-mask-rcnn-pytorch.html#.YsxbR-xBxH5
 
 class PedestrianDataset(torch.utils.data.Dataset):
     def __init__(self, root, transforms=None):
@@ -58,6 +60,15 @@ class PedestrianDataset(torch.utils.data.Dataset):
             xmax = np.max(pos[1])
             ymin = np.min(pos[0])
             ymax = np.max(pos[0])
+
+            if xmin > xmax:
+                xmin, xmax = xmax, xmin
+            if ymin > ymax:
+                ymin, ymin = ymax, ymin
+            if xmin == xmax:
+                print(mask)
+                print(self.masks[idx])
+
             boxes.append([xmin, ymin, xmax, ymax])
 
         boxes = torch.as_tensor(boxes, dtype=torch.float32)
@@ -113,6 +124,7 @@ def build_model(num_classes):
 
     return model
 
+
 from engine import train_one_epoch, evaluate
 import utils
 import transforms as T
@@ -128,56 +140,58 @@ def get_transform(train):
         transforms.append(T.RandomHorizontalFlip(0.5))
     return T.Compose(transforms)
 
-# use our dataset and defined transformations
-dataset = PedestrianDataset(DATASET, get_transform(train=True))
-dataset_test = PedestrianDataset(DATASET, get_transform(train=False))
 
-# split the dataset in train and test set
-torch.manual_seed(1)
-indices = torch.randperm(len(dataset)).tolist()
-dataset = torch.utils.data.Subset(dataset, indices[:SIZE])
-dataset_test = torch.utils.data.Subset(dataset_test, indices[SIZE:])
+if __name__ == '__main__':
+    print('Hola')
+    # use our dataset and defined transformations
+    dataset = PedestrianDataset(DATASET, get_transform(train=True))
+    dataset_test = PedestrianDataset(DATASET, get_transform(train=False))
 
-# define training and validation data loaders
-data_loader = torch.utils.data.DataLoader(
-    dataset, batch_size=1, shuffle=True, num_workers=4,
-    collate_fn=utils.collate_fn)
+    # split the dataset in train and test set
+    torch.manual_seed(1)
+    indices = torch.randperm(len(dataset)).tolist()
+    dataset = torch.utils.data.Subset(dataset, indices[:SIZE])
+    dataset_test = torch.utils.data.Subset(dataset_test, indices[SIZE:])
 
-data_loader_test = torch.utils.data.DataLoader(
-    dataset_test, batch_size=1, shuffle=False, num_workers=4,
-    collate_fn=utils.collate_fn)
+    # define training and validation data loaders
+    data_loader = torch.utils.data.DataLoader(
+        dataset, batch_size=1, shuffle=True, num_workers=4,
+        collate_fn=utils.collate_fn)
 
+    data_loader_test = torch.utils.data.DataLoader(
+        dataset_test, batch_size=1, shuffle=False, num_workers=4,
+        collate_fn=utils.collate_fn)
 
-device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    print(device)
+    # our dataset has two classes only - background and person
+    num_classes = 2
 
-# our dataset has two classes only - background and person
-num_classes = 2
+    # get the model using our helper function
+    model = build_model(num_classes)
+    # move model to the right device
+    model.to(device)
 
-# get the model using our helper function
-model = build_model(num_classes)
-# move model to the right device
-model.to(device)
+    # construct an optimizer
+    params = [p for p in model.parameters() if p.requires_grad]
+    optimizer = torch.optim.SGD(params, lr=0.005,
+                                momentum=0.9, weight_decay=0.0005)
 
-# construct an optimizer
-params = [p for p in model.parameters() if p.requires_grad]
-optimizer = torch.optim.SGD(params, lr=0.005,
-                            momentum=0.9, weight_decay=0.0005)
+    # and a learning rate scheduler which decreases the learning rate by
+    # 10x every 3 epochs
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer,
+                                                   step_size=3,
+                                                   gamma=0.1)
 
-# and a learning rate scheduler which decreases the learning rate by
-# 10x every 3 epochs
-lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer,
-                                               step_size=3,
-                                               gamma=0.1)
+    # number of epochs
+    num_epochs = 10
 
-# number of epochs
-num_epochs = 10
+    for epoch in range(num_epochs):
+        # train for one epoch, printing every 10 iterations
+        train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq=10)
+        # update the learning rate
+        lr_scheduler.step()
+        # evaluate on the test dataset
+        evaluate(model, data_loader_test, device=device)
 
-for epoch in range(num_epochs):
-    # train for one epoch, printing every 10 iterations
-    train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq=10)
-    # update the learning rate
-    lr_scheduler.step()
-    # evaluate on the test dataset
-    evaluate(model, data_loader_test, device=device)
-
-torch.save(model, MODELNAME)
+    torch.save(model, MODELNAME)
